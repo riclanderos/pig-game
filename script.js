bc7b82818d8d53cffabf9cd9faf6bed935f1c4a5
Ricardo Landeros <landerosr81@gmail.com>
2025-02-15 13:04:56 -0800

    switching to next player

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selected elements
+let player0Element = document.querySelector('.player--0');
+let player1Element = document.querySelector('.player--1');
 let score0Element = document.querySelector('#score--0');
 let score1Element = document.querySelector('#score--1');
 let current0Element = document.getElementById('current--0');
@@ -15,7 +17,9 @@ score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener('click', function () {
   let dice = Math.trunc(Math.random() * 6) + 1;
@@ -25,6 +29,14 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0Element.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
